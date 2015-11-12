891b55232f15f9a0a00533476101d952632e8271
John Crossman <johncrossman@berkeley.edu>
2015-11-12 14:32:24 -0800

    CLC-5972, fix angular misuse; TODO on apiFactory; less css

--- src/assets/javascripts/angular/controllers/widgets/studentLinkingController.js
+++ src/assets/javascripts/angular/controllers/widgets/studentLinkingController.js
@@ -5,19 +5,12 @@ var angular = require('angular');
 /**
  * Controller links student account with the requested delegate account
  */
-angular.module('calcentral.controllers').controller('StudentLinkingController', function(studentLinkingFactory, $scope) {
-  $scope.linkAccounts = function() {
-    return studentLinkingFactory.linkAccounts().success(function(data) {
-      angular.extend($scope, data);
-    }).error(function() {
-      $scope.displayError = 'failure';
-    });
+angular.module('calcentral.controllers').controller('StudentLinkingController', function($scope) {
+  $scope.getTermsAndConditions = function() {
+    // TODO: Properly wire this into studentLinkingFactory
+    $scope.agreement = {
+      text: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      showing: true
+    };
   };
-
-  var loadStudentLinkingCard = function() {
-    $scope.termsAndConditions = studentLinkingFactory.termsAndConditions();
-  };
-
-  loadStudentLinkingCard();
 });
-
--- src/assets/javascripts/angular/factories/studentLinkingFactory.js
+++ src/assets/javascripts/angular/factories/studentLinkingFactory.js
@@ -5,23 +5,12 @@ var angular = require('angular');
 /**
  * POST to the Campus Solutions API which links delegate to a student account
  */
-angular.module('calcentral.factories').factory('studentLinkingFactory', function(apiService, $http) {
-  // TODO: var urlTermsAndConditions = 'TBD';
-  var urlPostSecurityKey = 'TBD';
-
-  // Get text
+angular.module('calcentral.factories').factory('studentLinkingFactory', function($http) {
   var getTermsAndConditions = function() {
-    // TODO: return apiService.http.request(options, urlTermsAndConditions);
-    return 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-  };
-
-  // Post
-  var linkAccounts = function(options) {
-    return $http.post(urlPostSecurityKey, options);
+    return $http.get('/api/delegated_access/terms_and_conditions');
   };
 
   return {
-    termsAndConditions: getTermsAndConditions,
-    linkAccounts: linkAccounts
+    getTermsAndConditions: getTermsAndConditions
   };
 });
