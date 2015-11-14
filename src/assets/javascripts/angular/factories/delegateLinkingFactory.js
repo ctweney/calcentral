b00285f5d02c010e30585de68ae386c84a96ed0a
John Crossman <johncrossman@berkeley.edu>
2015-11-13 17:03:02 -0800

    CLC-5971, use  apiService.http.request()

--- src/assets/javascripts/angular/factories/delegateLinkingFactory.js
+++ src/assets/javascripts/angular/factories/delegateLinkingFactory.js
@@ -5,11 +5,11 @@ var angular = require('angular');
 /**
  * POST to the Campus Solutions API which links delegate to a student account
  */
-angular.module('calcentral.factories').factory('delegateLinkingFactory', function($http) {
+angular.module('calcentral.factories').factory('delegateLinkingFactory', function(apiService, $http) {
   var urlDelegateAccessAPI = '/api/campus_solutions/delegate_access';
 
-  var getTermsAndConditions = function() {
-    return $http.get(urlDelegateAccessAPI);
+  var getTermsAndConditions = function(options) {
+    return apiService.http.request(options, urlDelegateAccessAPI);
   };
   var postSecurityKey = function(options) {
     return $http.post(urlDelegateAccessAPI, options);
