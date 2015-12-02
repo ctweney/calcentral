cb13ca974dcb6a0c993fafb2ef2455ef9e39c8dc
Christian Vuerings <vueringschristian@gmail.com>
2015-12-02 14:19:20 -0800

    SISRP-11943 - Profile backend breakages are not indicated in UX
    https://jira.berkeley.edu/browse/SISRP-11943

--- src/assets/javascripts/angular/controllers/widgets/profile/contactController.js
+++ src/assets/javascripts/angular/controllers/widgets/profile/contactController.js
@@ -1,14 +1,21 @@
 'use strict';
 
 var angular = require('angular');
+var _ = require('lodash');
 
 /**
  * Contact controller
  */
 angular.module('calcentral.controllers').controller('ContactController', function(profileFactory, $scope) {
+  $scope.contactInformation = {
+    isLoading: true,
+    isErrored: false
+  };
+
   var loadInformation = function() {
-    profileFactory.getPerson().then(function() {
-      $scope.isLoading = false;
+    profileFactory.getPerson().then(function(data) {
+      $scope.contactInformation.isLoading = false;
+      $scope.contactInformation.isErrored = _.get(data, 'data.errored');
     });
   };
 
