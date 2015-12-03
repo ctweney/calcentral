f44fb4259cfda878c89b13c0e3a0031a1b24ba22
Christian Vuerings <vueringschristian@gmail.com>
2015-12-03 08:31:50 -0800

    SISRP-10752 - Profile - CS language data - Only 'native' surfaces in CalCentral
    https://jira.berkeley.edu/browse/SISRP-10752

--- src/assets/javascripts/angular/controllers/widgets/profile/languagesSectionController.js
+++ src/assets/javascripts/angular/controllers/widgets/profile/languagesSectionController.js
@@ -1,16 +1,39 @@
 'use strict';
 
+var _ = require('lodash');
 var angular = require('angular');
 
 /**
  * Language section controller
  */
 angular.module('calcentral.controllers').controller('LanguagesSectionController', function(profileFactory, $scope, $q) {
+  var levelMapping = {
+    native: 'native',
+    teach: 'teacher',
+    translate: 'translator'
+  };
+
+  var parseLevels = function(languages) {
+    languages = _.map(languages, function(language) {
+      if (!language.levels) {
+        language.levels = [];
+      }
+      _.each(levelMapping, function(value, key) {
+        if (language[key]) {
+          language.levels.push(value);
+        }
+      });
+      return language;
+    });
+    return languages;
+  };
+
   var parsePerson = function(data) {
     var person = data.data.feed.student;
+    var languages = parseLevels(person.languages);
     angular.extend($scope, {
       languages: {
-        content: person.languages
+        content: languages
       }
     });
   };
