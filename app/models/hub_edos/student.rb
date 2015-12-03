5243c5ab3546b67bc18a84078d688a03ce8e2868
John Crossman <johncrossman@berkeley.edu>
2015-12-02 16:11:40 -0800

    CLC-6066, less brittle HubEdos module via safe response handling

--- app/models/hub_edos/student.rb
+++ app/models/hub_edos/student.rb
@@ -2,8 +2,9 @@ module HubEdos
   class Student < Proxy
 
     include Cache::UserCacheExpiry
+    include ResponseHandler
 
-    SENSITIVE_KEYS = ['addresses', 'names', 'phones', 'emails', 'emergencyContacts']
+    SENSITIVE_KEYS = %w(addresses names phones emails emergencyContacts)
 
     def initialize(options = {})
       super(Settings.hub_edos_proxy, options)
@@ -27,15 +28,13 @@ module HubEdos
     end
 
     def transform_address_keys(response)
-      # this should really be done in the Integration Hub, but they won that argument due to time constraints.
-      if response['studentResponse'].present? && response['studentResponse']['students'].present?
-        response['studentResponse']['students']['students'].each do |student|
-          if student['addresses'].present?
-            student['addresses'].each do |address|
-              address['state'] = address.delete('stateCode')
-              address['postal'] = address.delete('postalCode')
-              address['country'] = address.delete('countryCode')
-            end
+      # TODO: this should really be done in the Integration Hub
+      get_students(response).each do |student|
+        if student['addresses'].present?
+          student['addresses'].each do |address|
+            address['state'] = address.delete('stateCode')
+            address['postal'] = address.delete('postalCode')
+            address['country'] = address.delete('countryCode')
           end
         end
       end
@@ -43,13 +42,11 @@ module HubEdos
     end
 
     def redact_sensitive_keys(response)
-      # more stuff the Integration Hub should be doing, but the team doesn't have time for.
-      if response['studentResponse'].present? && response['studentResponse']['students'].present?
-        response['studentResponse']['students']['students'].each do |student|
-          SENSITIVE_KEYS.each do |key|
-            if student[key].present?
-              student[key].delete_if { |k| k['uiControl'].present? && k['uiControl']['code'] == 'N' }
-            end
+      # TODO: more stuff the Integration Hub should be doing
+      get_students(response).each do |student|
+        SENSITIVE_KEYS.each do |key|
+          if student[key].present?
+            student[key].delete_if { |k| k['uiControl'].present? && k['uiControl']['code'] == 'N' }
           end
         end
       end
@@ -58,14 +55,14 @@ module HubEdos
 
     def filter_fields(response)
       # only include the fields that this proxy is responsible for
+      students = get_students(response)
+      first_student = students.any? ? students[0] : {}
       if include_fields.nil?
-        return response['studentResponse']['students']['students'][0]
+        return first_student
       end
       result = {}
-      if response['studentResponse'].present? && response['studentResponse']['students'].present?
-        response['studentResponse']['students']['students'][0].keys.each do |field|
-          result[field] = response['studentResponse']['students']['students'][0][field] if include_fields.include?(field)
-        end
+      first_student.keys.each do |field|
+        result[field] = first_student[field] if include_fields.include? field
       end
       result
     end
